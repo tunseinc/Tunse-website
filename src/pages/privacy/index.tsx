55b22499a6a697d39d11c4898d21b0afc0cba06b
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2025-06-10 16:33:23 +0100

    Updated the document for the homepage

--- src/pages/privacy/index.tsx
+++ src/pages/privacy/index.tsx
@@ -1,8 +1,24 @@
 import PrivacyDoc from '../../assets/pdfs/privacy_policy.pdf'
 
 const Privacy = () => {
+
   return (
-    <iframe src={PrivacyDoc} title={"privacy-policy"} className={"policy"} width={"100%"} height={"900px"}></iframe>
+    <div
+      style={{
+        width: "100%",
+        minHeight: "100vh",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src={PrivacyDoc}
+        title="privacy-policy"
+        style={{ width: "100%", height: "100vh", border: "none" }}
+        id="pdfEmbed"
+      />
+    </div>
   )
 }
 
--- src/pages/terms-and-conditions/index.tsx
+++ src/pages/terms-and-conditions/index.tsx
@@ -2,14 +2,23 @@ import TermsDoc from '../../assets/pdfs/terms.pdf'
 
 
 const TermsAndConditions = () => {
+
     return (
-        <iframe
-            src={TermsDoc}
-            title={"terms-and-condition"}
-            className={"policy"}
-            width={"100%"}
-            height={"900px"}
-        />
+        <div
+            style={{
+                width: "100%",
+                minHeight: "100vh",
+                margin: 0,
+                padding: 0,
+                overflow: "hidden",
+            }}
+        >
+            <iframe
+                src={TermsDoc}
+                title={"terms-and-condition"}
+                style={{ width: "100%", height: "100vh", border: "none" }}
+            />
+        </div>
     )
 }
 
