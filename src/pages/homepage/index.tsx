d95a3e57d84815ecab770a4d9dafc7593dd6f226
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2025-07-22 16:01:46 +0100

    Temporarily removed what-people-say-section

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -6,7 +6,6 @@ import Hero from "../../components/homepage/Hero"
 import HowItWorks from "../../components/homepage/HowItWorks"
 import ReliableService from "../../components/homepage/ReliableService"
 import Tools from "../../components/homepage/Tools"
-import WhatUsersSay from "../../components/homepage/WhatUsersSay"
 import Navbar from "../../components/shared/Navbar"
 
 const Homepage = () => {
@@ -19,7 +18,6 @@ const Homepage = () => {
                 <HowItWorks />
                 <Artisans />
                 <ReliableService />
-                <WhatUsersSay />
                 <Faq />
                 <Tools />
                 <Footer />
